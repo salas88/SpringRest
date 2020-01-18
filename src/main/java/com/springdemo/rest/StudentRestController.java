5575862d0252254e76ce45523e6c9370596862ee
Vladislav Hasporian <vladyslav.cartel@gmail.com>
2020-01-18 16:25:59 +0200

    add PostConstruct method for create and add objects

--- src/main/java/com/springdemo/rest/StudentRestController.java
+++ src/main/java/com/springdemo/rest/StudentRestController.java
@@ -2,6 +2,8 @@ package com.springdemo.rest;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.annotation.PostConstruct;
+
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,17 +15,21 @@ import com.springdemo.entity.Student;
 @RequestMapping("/api")
 public class StudentRestController {
 	
-	@GetMapping("/students")
-	public List<Student> showStudent(){
-		
-		List<Student> students = new ArrayList<>();
-		
+	private List<Student> students;
+	
+	@PostConstruct
+	public void loadDate() {
+		students = new ArrayList<>();
+
 		students.add(new Student("Vlad", "Hasporian"));
 		students.add(new Student("Gaga", "Kiknadze"));
 		students.add(new Student("Karim", "Mastan"));
+	}
+	
+	@GetMapping("/students")
+	public List<Student> showStudent(){
 		
 		return students;
 		
 	}
-
 }
