3b11bf0071341d69205e157b51996a6edbe1794c
Vladislav Hasporian <vladyslav.cartel@gmail.com>
2020-01-18 16:58:40 +0200

    add exception headler for vot_found and bad_request

--- src/main/java/com/springdemo/rest/StudentRestController.java
+++ src/main/java/com/springdemo/rest/StudentRestController.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,7 +39,23 @@ public class StudentRestController {
 	
 	@GetMapping("/students/{studentId}")
 	public Student getStudentForId(@PathVariable int studentId) {
-		
+		if( (studentId >= students.size()) || (studentId < 0)) {
+			throw new StudentNotFoundException("Student is not found " + studentId);
+		}
 		return students.get(studentId);
 	}
+	
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorRespons> handlerException(StudentNotFoundException exc){
+		
+		StudentErrorRespons error = new StudentErrorRespons();
+		
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setMessage(exc.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+		
+		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+		
+	}
+	
 }
